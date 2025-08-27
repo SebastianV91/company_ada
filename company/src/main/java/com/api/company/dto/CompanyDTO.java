4aaeb0d1540d100edb66de1e9fbadb341bb8fc1f
SebastianV91 <severgara91@hotmail.com>
2025-08-26 20:56:52 -0500

    first commit

---
+++ company/src/main/java/com/api/company/dto/CompanyDTO.java
@@ -0,0 +1,43 @@
+package com.api.company.dto;
+
+public class CompanyDTO {
+
+
+    private String codigo_company;
+    private String name_company;
+    private String description_company;
+
+    public CompanyDTO() {
+    }
+
+    public CompanyDTO(String codigo_company, String name_company, String description_company) {
+        this.codigo_company = codigo_company;
+        this.name_company = name_company;
+        this.description_company = description_company;
+    }
+
+    public String getCodigo_company() {
+        return codigo_company;
+    }
+
+    public void setCodigo_company(String codigo_company) {
+        this.codigo_company = codigo_company;
+    }
+
+    public String getName_company() {
+        return name_company;
+    }
+
+    public void setName_company(String name_company) {
+        this.name_company = name_company;
+    }
+
+    public String getDescription_company() {
+        return description_company;
+    }
+
+    public void setDescription_company(String description_company) {
+        this.description_company = description_company;
+    }
+
+}
